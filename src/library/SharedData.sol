d3506c2d8baed18246312a94a30545fff04724bf
obah <olusojiobah@gmail.com>
2024-11-14 19:04:21 +0100

    chore: added library with deployment addresses

---
+++ src/library/SharedData.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+library SharedData {
+    address constant ETH_SEPOLIA_CCM =
+        0x2f43668479e1F2B5955f9C9bf1A93123a6738AF0;
+    address constant LISK_SEPOLIA_CCM =
+        0x6193c44E7C8cE5438FEE9D2e94Ea98369Ef297af;
+}
