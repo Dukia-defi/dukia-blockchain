73b7a87728dd3bd5e740c9fadaa7db75f13a2c07
Abolare Roheemah <roheemahabo@gmail.com>
2024-11-19 15:33:36 +0100

    deployed aave contract again

--- contracts/src/utils/CrossChainMessenger.sol
+++ contracts/src/utils/CrossChainMessenger.sol
@@ -1,181 +1,181 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+// //SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.26;
 
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/utils/Pausable.sol";
+// import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+// import "@openzeppelin/contracts/utils/Pausable.sol";
 
-contract CrossChainMessenger is Pausable, ReentrancyGuard {
-    error UnsupportedChain();
-    error InvalidAddress();
-    error MessageProcessed();
-    error VerificationFailed();
-    error CallFailed();
-    error Unathorized();
+// contract CrossChainMessenger is Pausable, ReentrancyGuard {
+//     error UnsupportedChain();
+//     error InvalidAddress();
+//     error MessageProcessed();
+//     error VerificationFailed();
+//     error CallFailed();
+//     error Unathorized();
 
-    event MessageSent(
-        address to,
-        uint32 toChainId,
-        bytes messageData,
-        address indexed sender,
-        uint256 indexed messageId,
-        bytes32 indexed messageHash
-    );
+//     event MessageSent(
+//         address to,
+//         uint32 toChainId,
+//         bytes messageData,
+//         address indexed sender,
+//         uint256 indexed messageId,
+//         bytes32 indexed messageHash
+//     );
 
-    event ChainStatusUpdated(uint32 chainId, bool supported);
+//     event ChainStatusUpdated(uint32 chainId, bool supported);
 
-    event MessageReceived(
-        address indexed from,
-        uint32 fromChainId,
-        bytes message,
-        uint256 indexed messageId
-    );
+//     event MessageReceived(
+//         address indexed from,
+//         uint32 fromChainId,
+//         bytes message,
+//         uint256 indexed messageId
+//     );
 
-    struct Message {
-        uint32 sourceChainId;
-        uint32 destinationChainId;
-        address sender;
-        address recipient;
-        bytes message;
-    }
+//     struct Message {
+//         uint32 sourceChainId;
+//         uint32 destinationChainId;
+//         address sender;
+//         address recipient;
+//         bytes message;
+//     }
 
-    address public immutable OWNER;
+//     address public immutable OWNER;
 
-    uint256 public nonce;
+//     uint256 public nonce;
 
-    mapping(bytes32 => bool) public sentMessages;
-    mapping(uint32 => bool) public supportedChains;
-    mapping(bytes32 => bool) public processedMessages;
-    mapping(bytes32 => Message) public messages;
+//     mapping(bytes32 => bool) public sentMessages;
+//     mapping(uint32 => bool) public supportedChains;
+//     mapping(bytes32 => bool) public processedMessages;
+//     mapping(bytes32 => Message) public messages;
 
-    modifier onlyOwner() {
-        if (msg.sender != OWNER) revert Unathorized();
-        _;
-    }
+//     modifier onlyOwner() {
+//         if (msg.sender != OWNER) revert Unathorized();
+//         _;
+//     }
 
-    constructor() {
-        OWNER = msg.sender;
-    }
+//     constructor() {
+//         OWNER = msg.sender;
+//     }
 
-    //todo implement bridge later using across
-    // function bridge() external {};
+//     //todo implement bridge later using across
+//     // function bridge() external {};
 
-    function sendMessage(
-        address _userAddress,
-        uint32 _toChainId,
-        bytes memory _message,
-        address _toAddress
-    ) external payable returns (bytes32) {
-        if (!supportedChains[_toChainId]) revert UnsupportedChain();
-        if (_toAddress == address(0)) revert InvalidAddress();
+//     function sendMessage(
+//         address _userAddress,
+//         uint32 _toChainId,
+//         bytes memory _message,
+//         address _toAddress
+//     ) external payable returns (bytes32) {
+//         if (!supportedChains[_toChainId]) revert UnsupportedChain();
+//         if (_toAddress == address(0)) revert InvalidAddress();
 
-        nonce += 1;
+//         nonce += 1;
 
-        bytes32 messageHash = keccak256(
-            abi.encodePacked(
-                block.chainid,
-                _toChainId,
-                _userAddress,
-                _toAddress,
-                nonce,
-                _message
-            )
-        );
+//         bytes32 messageHash = keccak256(
+//             abi.encodePacked(
+//                 block.chainid,
+//                 _toChainId,
+//                 _userAddress,
+//                 _toAddress,
+//                 nonce,
+//                 _message
+//             )
+//         );
 
-        sentMessages[messageHash] = true;
+//         sentMessages[messageHash] = true;
 
-        Message storage message = messages[messageHash];
+//         Message storage message = messages[messageHash];
 
-        message.sourceChainId = uint32(block.chainid);
-        message.destinationChainId = _toChainId;
-        message.sender = _userAddress;
-        message.recipient = _toAddress;
-        message.message = _message;
+//         message.sourceChainId = uint32(block.chainid);
+//         message.destinationChainId = _toChainId;
+//         message.sender = _userAddress;
+//         message.recipient = _toAddress;
+//         message.message = _message;
 
-        emit MessageSent(
-            _toAddress,
-            _toChainId,
-            _message,
-            _userAddress,
-            nonce,
-            messageHash
-        );
+//         emit MessageSent(
+//             _toAddress,
+//             _toChainId,
+//             _message,
+//             _userAddress,
+//             nonce,
+//             messageHash
+//         );
 
-        return messageHash;
-    }
+//         return messageHash;
+//     }
 
-    function receiveMessage(
-        address _userAddress,
-        bytes calldata _message,
-        address _targetAddress,
-        uint256 _messageId,
-        uint32 _fromChainId,
-        bytes32 _messageHash
-    ) external payable nonReentrant whenNotPaused returns (bytes memory) {
-        if (!supportedChains[_fromChainId]) revert UnsupportedChain();
-        if (processedMessages[_messageHash]) revert MessageProcessed();
-        if (_targetAddress == address(0)) revert InvalidAddress();
+//     function receiveMessage(
+//         address _userAddress,
+//         bytes calldata _message,
+//         address _targetAddress,
+//         uint256 _messageId,
+//         uint32 _fromChainId,
+//         bytes32 _messageHash
+//     ) external payable nonReentrant whenNotPaused returns (bytes memory) {
+//         if (!supportedChains[_fromChainId]) revert UnsupportedChain();
+//         if (processedMessages[_messageHash]) revert MessageProcessed();
+//         if (_targetAddress == address(0)) revert InvalidAddress();
 
-        // bool verify = _verifyMessage(
-        //     _messageHash,
-        //     _fromChainId,
-        //     _userAddress,
-        //     _message
-        // );
+//         // bool verify = _verifyMessage(
+//         //     _messageHash,
+//         //     _fromChainId,
+//         //     _userAddress,
+//         //     _message
+//         // );
 
-        // if (!verify) revert VerificationFailed();
+//         // if (!verify) revert VerificationFailed();
 
-        processedMessages[_messageHash] = true;
+//         processedMessages[_messageHash] = true;
 
-        (bool success, bytes memory data) = _targetAddress.call{
-            value: msg.value
-        }(_message);
+//         (bool success, bytes memory data) = _targetAddress.call{
+//             value: msg.value
+//         }(_message);
 
-        if (!success) revert CallFailed();
+//         if (!success) revert CallFailed();
 
-        emit MessageReceived(_userAddress, _fromChainId, _message, _messageId);
+//         emit MessageReceived(_userAddress, _fromChainId, _message, _messageId);
 
-        return data;
-    }
+//         return data;
+//     }
 
-    function setSupportedChain(
-        uint32 chainId,
-        bool supported
-    ) external onlyOwner {
-        supportedChains[chainId] = supported;
+//     function setSupportedChain(
+//         uint32 chainId,
+//         bool supported
+//     ) external onlyOwner {
+//         supportedChains[chainId] = supported;
 
-        emit ChainStatusUpdated(chainId, supported);
-    }
+//         emit ChainStatusUpdated(chainId, supported);
+//     }
 
-    function pause() internal onlyOwner {
-        _pause();
-    }
+//     function pause() internal onlyOwner {
+//         _pause();
+//     }
 
-    function unpause() internal onlyOwner {
-        _unpause();
-    }
+//     function unpause() internal onlyOwner {
+//         _unpause();
+//     }
 
-    // function _verifyMessage(
-    //     bytes32 _messageHash,
-    //     uint32 _sourceChainId,
-    //     address _userAddress,
-    //     bytes calldata _message
-    // ) internal view returns (bool) {
-    //     Message storage message = messages[_messageHash];
+//     // function _verifyMessage(
+//     //     bytes32 _messageHash,
+//     //     uint32 _sourceChainId,
+//     //     address _userAddress,
+//     //     bytes calldata _message
+//     // ) internal view returns (bool) {
+//     //     Message storage message = messages[_messageHash];
 
-    //     return (message.sourceChainId == _sourceChainId &&
-    //         message.sender == _userAddress &&
-    //         keccak256(message.message) == keccak256(_message));
-    // }
+//     //     return (message.sourceChainId == _sourceChainId &&
+//     //         message.sender == _userAddress &&
+//     //         keccak256(message.message) == keccak256(_message));
+//     // }
 
-    function getFunctionBytes(
-        string memory _functionSig,
-        uint256 _param
-    ) external pure returns (bytes memory) {
-        bytes memory functionBytes = abi.encodeWithSignature(
-            _functionSig,
-            _param
-        );
+//     function getFunctionBytes(
+//         string memory _functionSig,
+//         uint256 _param
+//     ) external pure returns (bytes memory) {
+//         bytes memory functionBytes = abi.encodeWithSignature(
+//             _functionSig,
+//             _param
+//         );
 
-        return functionBytes;
-    }
-}
+//         return functionBytes;
+//     }
+// }
