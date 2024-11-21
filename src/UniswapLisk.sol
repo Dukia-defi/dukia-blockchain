5232d5ebbedff708d428f34438c260d21d0f554c
Angella-Mulikatete <mulikateteangella@gmail.com>
2024-11-21 12:19:22 +0300

    worked o uniswap lisk

--- src/UniswapLisk.sol
+++ src/UniswapLisk.sol
@@ -7,19 +7,6 @@ import "../lib/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 import "./library/SharedData.sol";
 import {CrossChainMessenger} from "./utils/CrossChainMessenger.sol";
 
-// interface IUniswapV2Router {
-//     function addLiquidity(
-//         address tokenA,
-//         address tokenB,
-//         uint amountADesired,
-//         uint amountBDesired,
-//         uint amountAMin,
-//         uint amountBMin,
-//         address to,
-//         uint deadline
-//     ) external returns (uint amountA, uint amountB, uint liquidity);
-// }
-
 contract UniswapTest {
     address userAddr = msg.sender;
     uint32 chainId = 11155111; // Sepolia chain ID
@@ -43,6 +30,28 @@ contract UniswapTest {
         );
 
         CrossChainMessenger(liskCCM).sendMessage(userAddr, chainId, message, target);
+    }
+
+
+    function addLiquidity( uint32 _toChainId,uint256 usdcAmount, uint256 daiAmount,uint256 slippagePercent) external {
+        // Calculate minimum amounts (e.g., 95% of desired amounts)
+     
+        uint256 minUsdcAmount = (usdcAmount * 95) / 100;
+        uint256 minDaiAmount = (daiAmount * 95) / 100;
+
+        bytes memory message = abi.encodeWithSignature(
+            "addLiquidity(address,address,uint256,uint256,uint256,uint256,address,uint256)",
+            USDC,
+            DAI,
+            usdcAmount,
+            daiAmount,
+            minUsdcAmount,
+            minDaiAmount,
+            msg.sender,
+            block.timestamp + 15
+        );
+
+         CrossChainMessenger(liskCCM).sendMessage(userAddr, chainId, message,target);
     }
 }
 
